2735e16015581dd2095344460a72c03c9c874c30
Naitik Soni <83502368+Naitik-Soni@users.noreply.github.com>
2023-06-24 18:50:12 +0530

    Created Given the marks of N students in an Array A, calculate the mean.py

---
+++ Given the marks of N students in an Array A, calculate the mean.py
@@ -0,0 +1,28 @@
+'''
+---Given the marks of N students in an Array A, calculate the mean---
+Input:
+N = 4 
+A = { 56 , 67 , 30 , 79 }
+
+Output:
+58
+
+Explanation:
+56+67+30+79 = 232;  232/4 = 58.
+So, the Output is 58.
+'''
+class Solution:
+    def mean(self, N , A):
+        sum = 0
+        for i in A:
+            sum += i
+        
+        return int(sum/N)
+
+obj = Solution()
+N = int(input())
+A = []
+for k in range(N):
+    A.append(int(input()))
+
+print(obj.mean(N,A))
