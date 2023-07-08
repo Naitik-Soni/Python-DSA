3c807457695b1f3217a1c93054cf89faedd6541d
Naitik Soni <83502368+Naitik-Soni@users.noreply.github.com>
2023-07-08 18:03:49 +0530

    Create Given a number, the task is to check if it is tidy or not.py

---
+++ Given a number, the task is to check if it is tidy or not.py
@@ -0,0 +1,37 @@
+'''
+Program:
+Given a number, the task is to check if it is tidy or not. A tidy number is a number whose digits are in non-decreasing order.
+
+Input:
+1234
+
+Output:
+Yes
+
+Input:
+1243
+
+Output:
+No
+
+Explanation:
+Digits “4” and “3” violate the property.
+'''
+n = 1353
+a = n
+tidy = 1
+
+while a>0:
+    k = a%10
+    a = a//10
+    s = a%10
+    if s<=k:
+        tidy = 1
+    else:
+        tidy = 0
+        break
+    
+if tidy == 1:
+    print("Number is Tidy")
+else:
+    print("Number is not Tidy")
